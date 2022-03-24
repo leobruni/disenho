be5e983e5513f9f99cb90f73be33eef48ca70596
leobruni <jleobruni@gmail.com>
2022-03-23 23:03:43 -0300

    Compartir metodos entre objetos

--- archivo-ejemplo.js
+++ archivo-ejemplo.js
@@ -1,1 +1,15 @@
-	hola
+//Compartir metodos entre objetos
+
+function getEdad(){
+	return this.edad;
+};
+
+//Objetos
+
+var obj1 = {
+	nombre: 'nombre_objeto',
+	edad: 23,
+	getEdad: getEdad
+};
+
+

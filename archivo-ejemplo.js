039708c108988acbf22855276eaeab69db3d6695
leobruni <jleobruni@gmail.com>
2022-03-31 00:00:18 -0300

    Agregamos Funciones callbacks

--- archivo-ejemplo.js
+++ archivo-ejemplo.js
@@ -42,8 +42,10 @@ var obj1 = {
 
 //Definir clases
 function Persona(nombre,pass) {
+
 	this.nombre = nombre || 'Defaul'; //Constructor con valores default
 	this.pass = pass || 'Default Pass';
+	}
 }
 //Los valores default se definen en un constructor para que cuando el primer valores(nombre o pass) no esten definidos le asigne un valor predeterminado y asi el programa no explote
 
@@ -59,6 +61,17 @@ var kike = new Persona('kike','1234');
 var audi = new Auto('audi','negro','v6');
 
 
-
 //Las funciones de constructor no retornan nada
 //Todas las funciones constructora de clases empiezan con mayusculas
+
+//-------------CALLBACKS---------------
+//Los callbacks son funciones que reciben como parametros una funcion
+
+function saludar(usuario){
+	return('hola ' + usuario);
+}
+
+function crearSaludo(usuario,saludar) {//funcionn callback, se ingresa como parametro la funcion y retorna el resultado de esta
+	return saludar(usuario);
+}
+

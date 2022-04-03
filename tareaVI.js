eb1819ebba76d7fb8d381e38213c55a3044f8260
leobruni <jleobruni@gmail.com>
2022-04-03 19:34:47 -0300

    avance 1 VI

--- tareaVI.js
+++ tareaVI.js
@@ -1,20 +1,30 @@
-/ Do not change any of the function names
+// Do not change any of the function names
 
 function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
+  return nombre.charAt(0).toUpperCase() + nombre.slice(1);
+  //charAt() Selecciona el caracter de la cadena en la posicion definida dentro de los parentesis.
+  //toUpperCase() Lleva a mayusculas la cadena
+  // slice() corta la cadena desde la posicion indicada en el parentesis.
 }
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
+  
+  return 'hola '+cb();
+  //La funcian de callback se tiene que introducir como parametro.
+  //invocarCallback(function(){return 'Mi nombre es callback function'})
 }
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+  return cb(n1,n2);
+  //operacionMatematica(5,3,function(a,b){return a+b});
 }
 
 function sumarArray(numeros, cb) {
@@ -22,12 +32,35 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+  let acum = [];
+  let sumT = 0;
+
+  for (let i = 0; i < numeros.length; i++) {
+    if(isNaN(numeros[i])){
+      console.log('Elemento ingresado no es un numero');
+      continue;
+    }else {
+      acum.push(numeros[i]);
+      console.log('numero agregado');
+    }
+  }
+  for (let j = 0; j < acum.length; j++) {
+    sumT = sumT + acum[j];
+  }
+  return cb(sumT);
+  //sumarArray([1,true,Boolean,2,'asd',5],function(sumT){return 'Suma de los numeros= '+sumT;});
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+  let acum = [];
+
+  for (let i = 0; i < array.length; i++) {
+    console.log(array[i]);
+    cb(array[i]);
+  }
 }
 
 function map(array, cb) {
@@ -45,7 +78,7 @@ function filter(array) {
 
 // No modificar nada debajo de esta línea
 // --------------------------------
-
+/*
 module.exports = {
   mayuscula,
   invocarCallback,
@@ -55,3 +88,4 @@ module.exports = {
   map,
   filter
 };
+*/
